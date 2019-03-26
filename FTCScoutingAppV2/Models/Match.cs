a5630f78978e867cd173e02a7f18398d908dcc0f
Mihnea Theodor Visoiu <visoiu.mihnea@gmail.com>
2019-03-26 12:59:23 +0200

    Reverted a mistaken deletion

---
+++ FTCScoutingAppV2/Models/Match.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FTCScoutingAppV2.Models
+{
+    public enum MatchTypes
+    {
+        Qualification, Elimination
+    }
+
+    public class Match
+    {
+        [Key]
+        public int ID { get;set;}
+        
+        [Display(Name ="Match type")]
+        public MatchTypes? matchType { get;set;}
+        [Display(Name ="Match number")]
+        public string matchNumber { get;set;}
+
+        public string teamID { get;set;}
+    }
+}
