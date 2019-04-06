fc8c72801d20bfc6fd6e23a3e6920b33b5dd330a
Mihnea Theodor Visoiu <visoiu.mihnea@gmail.com>
2019-04-06 16:53:03 +0300

    Added actual team number instead of routing id to match page

--- FTCScoutingAppV2/Pages/Matches/Index.cshtml.cs
+++ FTCScoutingAppV2/Pages/Matches/Index.cshtml.cs
@@ -19,16 +19,24 @@ namespace FTCScoutingAppV2.Pages.Matches
         public IList<Team> Teams { get; set; }
         public IList<Match> Matches { get; set; }
         public string routingID { get; set; }
+        public string teamID { get;set;}
 
         public async Task OnGetAsync()
         {
             AllMatches = await _context.Match.ToListAsync();
+            Teams = await _context.Team.ToListAsync();
             Matches = new List<Match>();
             routingID = HttpContext.Request.Query["id"];
+            teamID = string.Empty;
             foreach (var match in AllMatches)
             {
                 if (match.teamID == routingID)
                     Matches.Add(match);
+            }
+            foreach (var team in Teams)
+            {
+                if (team.ID.ToString() == routingID)
+                    teamID = team.teamID;
             }
         }
     }
