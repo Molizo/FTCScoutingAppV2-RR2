39a3626da6c0ec11c847ab6fcb7716f7482b00d7
Mihnea Theodor Visoiu <visoiu.mihnea@gmail.com>
2019-04-06 11:01:46 +0300

    Updated team model

--- FTCScoutingAppV2/Models/Team.cs
+++ FTCScoutingAppV2/Models/Team.cs
@@ -6,18 +6,52 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FTCScoutingAppV2.Models
 {
+    public enum StartLocations
+    {
+        Crater,Depot
+    }
+
+    public enum EndLocations
+    {
+        Latched,Fully,Partial,None
+    }
+
     public class Team
     {
         [Key]
         public int ID { get;set;}
 
-        [Display(Name = "Team ID")]
+        [Display(Name = "Team ID",ShortName ="ID")]
         public string teamID { get;set;}
-        [Display(Name = "Team name")]
+        [Display(Name = "Team name",ShortName ="Name")]
         public string teamName { get;set;}
-        [Display(Name = "Team location")]
+        [Display(Name = "Team location",ShortName ="Location")]
         public string teamLocation { get;set;}
         
+        [Display(Name ="Expected points",ShortName ="Exp PTS")]
+        public int ExpPTS { get;set;}
+        [Display(Name = "Average points", ShortName = "Avg PTS")]
+        public int AvgPTS { get; set; }
+        [Display(Name = "OPR points", ShortName = "OPR")]
+        public int OPR { get; set; }
+
+        [Display(Name ="Preferred starting location",ShortName ="Str loc")]
+        public StartLocations? startLocation { get;set;}
+
+        [Display(Name ="Robot lands",ShortName ="LND")]
+        public bool landing { get;set;}
+        [Display(Name = "Does sampling",ShortName ="SMP")]
+        public bool sampling { get; set; }
+        [Display(Name = "2nd sampling",ShortName ="2nd SMP")]
+        public bool doubleSampling { get; set; }
+        [Display(Name = "Places team marker",ShortName ="MRK")]
+        public bool teamMarker { get; set; }
+        [Display(Name = "Robot parks",ShortName ="PRK")]
+        public bool parking { get; set; }
+
+        [Display(Name = "Ending location", ShortName = "End loc")]
+        public EndLocations? endLocation { get; set; }
+
         [Display(Name ="Comments")]
         [DataType(DataType.MultilineText)]
         public string comments { get;set;}
