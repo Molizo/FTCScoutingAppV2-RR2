37fc664b733a2b40471f82e582070d57d9d32246
Mihnea Theodor Visoiu <visoiu.mihnea@gmail.com>
2019-04-06 11:11:31 +0300

    Added migrations for updated models

--- FTCScoutingAppV2/Data/Migrations/20190406081026_UpdatedModelsForRoverRuckus.Designer.cs
+++ FTCScoutingAppV2/Data/Migrations/20190406081026_UpdatedModelsForRoverRuckus.Designer.cs
@@ -4,14 +4,16 @@ using FTCScoutingAppV2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FTCScoutingAppV2.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190406081026_UpdatedModelsForRoverRuckus")]
+    partial class UpdatedModelsForRoverRuckus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
